ed91a9858775fa9a6c2cda7b7672e774b9b23b6a
Rajib <43757878+MRajibH@users.noreply.github.com>
2019-10-31 22:52:13 +0600

    Update guess.py

--- guess.py
+++ guess.py
@@ -2,9 +2,9 @@ import random
 
 guess_count = 1
 score = 1000;
-real_number = random.randint(0,100)
+real_number = random.randint(0,1000)
 #print(f'Number is {real_number}')
-get_number = int(input("Guess a number between 0 to 100 : "))
+get_number = int(input("Guess a number between 0 to 1000 : "))
 def guess():
     guesses = int(input("Enter Again: "))
     return guesses
@@ -20,6 +20,6 @@ while real_number != get_number:
         get_number = guess()
         score -= 10
         guess_count += 1
-if real_number is get_number:
+if real_number == get_number:
     print(f"Congratulations ! You've guessed the number correctly at {guess_count} attempt")
     print(f'And your Score is = {score} out of 1000')
